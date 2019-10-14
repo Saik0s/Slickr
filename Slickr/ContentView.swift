8d8b3187d9fd22fb9e2f2a86e06f6577135c1c75
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2019-10-14 20:28:36 +0200

    Initial Commit

---
+++ Slickr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slickr
+//
+//  Created by Igor Tarasenko on 14/10/2019.
+//  Copyright © 2019 Igor Tarasenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
